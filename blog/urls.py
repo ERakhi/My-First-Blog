107a27e81436b59b22b736af0018e47725c4ea80
ERakhi <khanrakhi05@gmail.com>
2021-05-18 20:02:10 +0530

    Added delete button and publish button

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 # from django.conf.urls import url
 from django.urls import path
-from .views import post_list, post_detail, post_new, post_edit
+from .views import post_list, post_detail, post_new, post_edit, post_draft_list, post_publish, post_remove
 
 urlpatterns = [
     path('',post_list, name='post_list'),
     path('post/<int:pk>/',post_detail, name='post_detail'),
     path('post/new',post_new,name='post_new'),
     path('post/<int:pk>/edit/', post_edit, name='post_edit'),
+    path('drafts/',post_draft_list,name='post_draft_list'),
+    path('post/<pk>/publish/', post_publish, name='post_publish'),
+    path('post/<pk>/remove/', post_remove, name='post_remove'),
+    
 ]
